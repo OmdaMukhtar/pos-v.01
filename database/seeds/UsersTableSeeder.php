6d28b808ddf85c005f14a1325799dd9d4559d631
omda <abuoop123@gmail.com>
2018-09-14 03:38:14 +0300

    config Appserviceprovider and userstableseeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,33 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        \DB::table('users')->delete();
+        \DB::table('users')->insert(array(
+            0 =>
+            array(
+                'id' => 1,
+                'name'=>'{"ar":"admin"}',
+                'email'=>'admin@admin.com',
+                'password' => '$2y$10$hWhJmthPjwUq9riUg7Q9Ju8ODUFcg5AHf9eTNAYNT64emfr.yhvpK',
+                'email_verified_at'=> '2018-07-13 02:23:10',
+            ),
+            1 =>
+            array(
+                'id' => 2,
+                'name'=>'{"ar":"omda"}',
+                'email'=>'omda@gmail.com',
+                'password' => '$2y$10$hWhJmthPjwUq9riUg7Q9Ju8ODUFcg5AHf9eTNAYNT64emfr.yhvpK',
+                'email_verified_at'=> '2018-07-13 02:23:10',
+            ),
+            2 =>
+            array(
+                'id' => 3,
+                'name'=>'{"ar":"testing"}',
+                'email'=>'test@gmail.com',
+                'password' => '$2y$10$hWhJmthPjwUq9riUg7Q9Ju8ODUFcg5AHf9eTNAYNT64emfr.yhvpK',
+                'email_verified_at'=> '2018-07-13 02:23:10',
+            ),
+        ));
+
     }
 }
